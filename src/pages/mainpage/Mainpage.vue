78f94706bbccff2a8adc3cfff845289e0ac84c1e
hs06146 <0_030_@naver.com>
2025-05-21 10:53:24 +0900

    refactor: warning 수정

--- src/pages/mainpage/Mainpage.vue
+++ src/pages/mainpage/Mainpage.vue
@@ -904,6 +904,7 @@ h3 {
   margin: 0 0 3rem 0;
   display: -webkit-box;
   -webkit-line-clamp: 2;
+  line-clamp: 2;
   -webkit-box-orient: vertical;
   overflow: hidden;
   flex: 1;
--- src/pages/mypage/Mypage.vue
+++ src/pages/mypage/Mypage.vue
@@ -494,6 +494,7 @@ onMounted(() => {
   margin: 0 0 1rem 0;
   display: -webkit-box;
   -webkit-line-clamp: 2;
+  line-clamp: 2;
   -webkit-box-orient: vertical;
   overflow: hidden;
 }
